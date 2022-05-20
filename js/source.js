233ac32975484cdccdb4425427dfd98df4910eff
Cygnet717 <kathybradt717@gmail.com>
2022-05-20 15:36:51 -0500

    linking from one page to another is functional

---
+++ js/source.js
@@ -0,0 +1,23 @@
+
+//register functionality
+let registerUser = (e) => {
+  e.preventDefault();
+  //create new user in db and send on to bulk add events
+  //for now send on to logged in user page
+  window.location.replace('userPage.html')
+}
+
+//login functionality
+let loginUser = (e) => {
+  e.preventDefault()
+  //validate user info using backend
+  window.location.replace('userPage.html')
+}
+
+//main user page functionality
+let userFName = 'Frodo'
+let userLName = 'Baggins'
+const greetingBox = document.getElementById('greetingBox')
+
+greetingBox.append(userFName + " " + userLName)
+
